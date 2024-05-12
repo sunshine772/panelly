e7298623cc78074c089f8618d5adbbfe309d0563
sunshine772 <nestor2397@gmail.com>
2024-05-12 14:24:20 -0400

    feat:

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        
+        // Forzar el uso de HTTPS en las URL generadas por Laravel
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -16,8 +17,5 @@ module.exports = {
         },
     },
 
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography')
-    ],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
